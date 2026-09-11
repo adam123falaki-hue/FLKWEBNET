798f27ce37fb598673278dfbfa1b5bc3f27df2db
adam123falaki-hue <adam123falaki@gmail.com>
2026-09-11 04:46:50 +0200

    Update ProjectEstimatorModal.tsx

--- app/components/ProjectEstimatorModal.tsx
+++ app/components/ProjectEstimatorModal.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import ProjectEstimator from '@/app/components/ProjectEstimator';
+import ProjectEstimator from './ProjectEstimator';
 
 interface ModalProps {
   isOpen: boolean;
